9c00d1d387553d91fb2b055ea4c758e0d5fd457b
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-08-04 21:53:53 +0700

    update css

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,42 @@
 "use client"; // This is a client component 👈🏽
 
-import { useRef,useState } from "react";
+import { useRef, useState } from "react";
 
 import Image from "next/image";
-import { useIsomorphicLayoutEffect } from "../utils";
 import Footer from "../components/Footer";
 import Head from "next/head";
 import Button from "../components/Button";
 import Link from "next/link";
 
 import Socials from "../components/Socials";
-import data from "../data/portfolio.json";
+import portfolioData from "../data/portfolio.json";
 import { Inter } from "next/font/google";
 
+// Import styles (assumed to be in a CSS module or a global stylesheet)
+import styles from "../styles/globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
 
-const Home = ({ subsets: ["latin"] }) => {
+const Home = () => {
   const data = {
     showCursor: true,
     name: 'My Page Title'
   };
   return (
     <div className={`relative ${data.showCursor ? "cursor-none" : ""}`}>
-      {data.showCursor }
       <Head>
         <title>{data.name}</title>
       </Head>
 
-      <div className={style.gradientcircle}></div>
-      <div className={style.gradientcirclebottom}></div>
-      
-      <div className="container mx-auto mb-10"></div>
+      <div className={styles.gradientcircle}></div>
+      <div className={styles.gradientcirclebottom}></div>
+
+      <div className="container mx-auto mb-10">
+        {/* Add your content here */}
+      </div>
 
     </div>
   );
 };
+
 export default Home;
