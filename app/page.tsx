8086c1a44dc3f88a28094f791d93c651938d1acc
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-08-04 21:51:05 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,28 +8,27 @@ import Footer from "../components/Footer";
 import Head from "next/head";
 import Button from "../components/Button";
 import Link from "next/link";
-import Cursor from "../components/Cursor";
-import Header from "../components/Header";
-import ServiceCard from "../components/ServiceCard";
+
 import Socials from "../components/Socials";
-import WorkCard from "../components/WorkCard";
 import data from "../data/portfolio.json";
+import { Inter } from "next/font/google";
 
 
-const Home = () => {
+
+const Home = ({ subsets: ["latin"] }) => {
   const data = {
     showCursor: true,
     name: 'My Page Title'
   };
   return (
     <div className={`relative ${data.showCursor ? "cursor-none" : ""}`}>
-      {data.showCursor && <Cursor />}
+      {data.showCursor }
       <Head>
         <title>{data.name}</title>
       </Head>
 
-      <div className="gradient-circle"></div>
-      <div className="gradient-circle-bottom"></div>
+      <div className={style.gradientcircle}></div>
+      <div className={style.gradientcirclebottom}></div>
       
       <div className="container mx-auto mb-10"></div>
 
