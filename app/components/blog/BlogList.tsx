f76f24e96e866aecb224b1d2c64c2917b133e1c2
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-08-06 18:03:26 +0700

    update

--- app/components/blog/BlogList.tsx
+++ app/components/blog/BlogList.tsx
@@ -9,7 +9,7 @@ const BlogList = ({ posts }) => {
         <div>
             <ul className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8 xl:px-60 px-8 ">
                 {posts.map((post) => (
-                    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+                    <li key="{post.id}" className="relative flex flex-col sm:flex-row xl:flex-col items-start">
                         <div className="order-1 sm:ml-6 xl:ml-0">
                             <h3 className="mb-1 text-slate-900 font-semibold">
                                 <span className="mb-1 block text-sm leading-6 text-indigo-500">Headless UI</span>{post.judul}
