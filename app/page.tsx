36b9f7836ac7d8c7ed2958544528f9a0f26472d9
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-08-06 18:09:52 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import react from "react";
 import Image from "next/image";
 import  Navigation  from "../app/components/navbar";
-import  PageOne  from "../app/components/page-one";
 import "./public/global.css";
 import Homebg from './assets/home/home-bg.jpg'; // Adjust the path as needed
 import profile from "./public/myprofil.webp";
